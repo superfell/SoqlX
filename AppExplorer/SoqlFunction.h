687c53ed0e5218880f3f4fbbae15b986b4cdd8d2
Simon Fell <fellforce@gmail.com>
2021-04-27 15:33:36 -0700

    add types to describe functions

--- AppExplorer/SoqlFunction.h
+++ AppExplorer/SoqlFunction.h
@@ -6,11 +6,21 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "SoqlToken.h"
 
-NS_ASSUME_NONNULL_BEGIN
+@class CaseInsensitiveStringKey;
 
-@interface SoqlFunction : NSObject
-
+@interface SoqlFuncArg : NSObject
++(instancetype)arg:(TokenType)type ex:(NSString*)ex;
+@property (assign,nonatomic) TokenType type;
+@property (strong,nonatomic) NSString *exampleValue;
 @end
 
-NS_ASSUME_NONNULL_END
+@interface SoqlFunction : NSObject
++(NSDictionary<CaseInsensitiveStringKey*,SoqlFunction*>*)all;
+
++(instancetype)fn:(NSString*)name args:(NSArray<SoqlFuncArg*>*)args;
+@property (strong,nonatomic) NSString *name;
+@property (strong,nonatomic) NSArray<SoqlFuncArg*>* args;
+@end
+
--- AppExplorer/SoqlFunction.m
+++ AppExplorer/SoqlFunction.m
@@ -6,7 +6,39 @@
 //
 
 #import "SoqlFunction.h"
+#import "CaseInsensitiveStringKey.h"
+
+@implementation SoqlFuncArg
++(instancetype)arg:(TokenType)type ex:(NSString*)ex {
+    SoqlFuncArg *a = [self new];
+    a.type = type;
+    a.exampleValue = ex;
+    return a;
+}
+@end
 
 @implementation SoqlFunction
++(NSDictionary<CaseInsensitiveStringKey*,SoqlFunction*>*)all {
+    NSArray<SoqlFunction*>* fns = @[
+        [self fn:@"MIN" args:@[[SoqlFuncArg arg:TTFieldPath ex:@"CreatedDate"]]],
+        [self fn:@"MAX" args:@[[SoqlFuncArg arg:TTFieldPath ex:@"CreatedDate"]]],
+        [self fn:@"FORMAT" args:@[[SoqlFuncArg arg:TTFieldPath ex:@"CreatedDate"]]],
+        [self fn:@"DISTANCE" args:@[[SoqlFuncArg arg:TTFieldPath ex:@"Location__c"],
+                                    [SoqlFuncArg arg:TTFunc ex:@"GEOLOCATION(1.0,1.0)"],
+                                    [SoqlFuncArg arg:TTLiteral ex:@"\'mi\'"]]],
+        [self fn:@"GEOLOCATION" args:@[[SoqlFuncArg arg:TTLiteral ex:@"1.0"], [SoqlFuncArg arg:TTLiteral ex:@"1.0"]]]
+    ];
+    NSMutableDictionary *d = [NSMutableDictionary dictionaryWithCapacity:fns.count];
+    for (SoqlFunction *f in fns) {
+        [d setObject:f forKey:[CaseInsensitiveStringKey of:f.name]];
+    }
+    return [NSDictionary dictionaryWithDictionary:d];
+}
 
++(instancetype)fn:(NSString*)name args:(NSArray<SoqlFuncArg*>*)args {
+    SoqlFunction *f = [self new];
+    f.name = name;
+    f.args = args;
+    return f;
+}
 @end
